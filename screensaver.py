a0b721ed834d7b7ab5fe54d1312a3149932b09ef
justcop <justcop@gmail.com>
2021-01-30 16:34:06 +0000

    Update screensaver.py

--- screensaver.py
+++ screensaver.py
@@ -15,10 +15,11 @@ def main():
     epd.init()
 
     black = Image.open(os.path.join("resources", "black.png"))
+    red = Image.open(os.path.join("resources", "red.png"))
     white = Image.open(os.path.join("resources", "white.png"))
     epd.display(epd.getbuffer(black),epd.getbuffer(white))
     logging.info("Display black")
-    epd.display(epd.getbuffer(white),epd.getbuffer(black))
+    epd.display(epd.getbuffer(white),epd.getbuffer(red))
     logging.info("Display red")
     epd.display(epd.getbuffer(white),epd.getbuffer(white))
     logging.info("Display white")
