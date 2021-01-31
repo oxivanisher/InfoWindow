4e8ccb67a1255ef0613712fb4366fc1d9705fa89
justcop <justcop@gmail.com>
2021-01-31 23:40:51 +0000

    Update infowindow.py

--- infowindow.py
+++ infowindow.py
@@ -172,7 +172,9 @@ def main():
     for grocy_item in grocy_items:
         (np_x, np_y) = red.getFont(tasks_font).getsize(str(grocy_item['days']))
         if int(grocy_item['days']) < 3:
-            red.text(298, (current_task_y + infowindow_opts["cell_spacing"]), str(grocy_item['days']) + red.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x), tasks_font)
+            red.text(298, (current_task_y + infowindow_opts["cell_spacing"]), str(grocy_item['days']), tasks_font)
+            red.rtext(583, (current_task_y + infowindow_opts["cell_spacing"]), red.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x), tasks_font)
+            
         else:
             black.text(298, (current_task_y + infowindow_opts["cell_spacing"]), black.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286), tasks_font)
         red.line(298, (current_task_y + line_height + 1), 582, (current_task_y + line_height + 1))
