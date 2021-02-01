7438f85b0aab95ceb91d0a46c7f31bb0b56bcf6c
justcop <justcop@gmail.com>
2021-02-01 00:50:04 +0000

    Update infowindow.py

--- infowindow.py
+++ infowindow.py
@@ -170,16 +170,19 @@ def main():
 
     current_task_y = 25
     for grocy_item in grocy_items:
-        (np_x, np_y) = red.getFont(tasks_font).getsize(str(grocy_item['days']))
+        (np_x, np_y) = red.getFont(tasks_font).getsize(str(grocy_item['days']) + " ")
         if int(grocy_item['days']) < 3:
-            red.text(298, (current_task_y + infowindow_opts["cell_spacing"]), str(grocy_item['days']), tasks_font)
-            (op_x, op_y) = red.getFont(tasks_font).getsize(red.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x))
-            red.text(583 - op_x, (current_task_y + infowindow_opts["cell_spacing"]), red.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x), tasks_font)
-            
+            text = red
         else:
-            black.text(298, (current_task_y + infowindow_opts["cell_spacing"]), str(grocy_item['days']), tasks_font)
-            (op_x, op_y) = black.getFont(tasks_font).getsize(black.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x))
-            black.text(583 - op_x, (current_task_y + infowindow_opts["cell_spacing"]), black.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x), tasks_font)
+            text = black
+            #red.text(298, (current_task_y + infowindow_opts["cell_spacing"]), str(grocy_item['days']) + " ", tasks_font)
+            #(op_x, op_y) = red.getFont(tasks_font).getsize(red.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x))
+            #red.text(583 - op_x, (current_task_y + infowindow_opts["cell_spacing"]), red.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x), tasks_font)
+            
+        #else:
+        text.text(298, (current_task_y + infowindow_opts["cell_spacing"]), str(grocy_item['days'] + " "), tasks_font)
+        (op_x, op_y) = text.getFont(tasks_font).getsize(black.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x))
+        text.text(583 - op_x, (current_task_y + infowindow_opts["cell_spacing"]), text.truncate(grocy_item['content'].encode(charset).strip(), tasks_font, 286 - np_x), tasks_font)
         red.line(298, (current_task_y + line_height + 1), 582, (current_task_y + line_height + 1))
 
 
