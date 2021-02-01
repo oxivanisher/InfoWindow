78175d6d02e247528483b950bb7d52bc7421e390
justcop <justcop@gmail.com>
2021-02-01 22:28:36 +0000

    Update infowindow.py

--- infowindow.py
+++ infowindow.py
@@ -135,7 +135,7 @@ def main():
 
     current_task_y = 25
     for todo_item in todo_items:
-        if todo_item['due']
+        if todo_item['due']:
          due = datetime.datetime(todo_item['due']) #[1:4],todo_item['due'][6:7],todo_item['due'][9:10])
          logging.info(str(due))
        # if datetime.datetime.now() - due >= 0:
